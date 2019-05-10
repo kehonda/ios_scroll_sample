c49e67f8061ffd67e93f66a44ff6ed848a552a46
kehonda <731.kentaro@gmail.com>
2019-05-10 09:12:40 +0900

    Initial Commit

---
+++ uiscrollview_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiscrollview_sample
+//
+//  Created by port-kentaro-honda on 2019/05/10.
+//  Copyright © 2019 port-kentaro-honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ uiscrollview_sampleTests/uiscrollview_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  uiscrollview_sampleTests.swift
+//  uiscrollview_sampleTests
+//
+//  Created by port-kentaro-honda on 2019/05/10.
+//  Copyright © 2019 port-kentaro-honda. All rights reserved.
+//
+
+import XCTest
+@testable import uiscrollview_sample
+
+class uiscrollview_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
